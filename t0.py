fd3a0c4bd34c31ff71b32b777a46407a27603bde
David Schweizer <david.schweizer@nhlstenden.com>
2023-08-31 15:21:40 +0200

    initital 2

--- t0.py
+++ t0.py
@@ -1,33 +1,60 @@
-from rich.syntax import Syntax
-from rich.table import Table
 
+from textual.containers import Horizontal
 from textual import events
 from textual.app import App, ComposeResult
-from textual.widgets import RichLog, Button
+from textual.screen import Screen
+from textual.widgets import RichLog, Button, Log, Static, Header, Footer, Placeholder
 
-class Terminal(RichLog):
-    def compose(self)->ComposeResult:
-        yield RichLog(highlight=True, markup=True)
-        yield Button('OK')
+# class Terminal(Static):
+#     def compose(self)->ComposeResult:
+#         yield Log()
  
-    def on_key(self, event: events.Key) -> None:
-        """Write Key events to log."""
-        text_log = self.query_one(RichLog)
-        text_log.write(event)
+#     def on_key(self, event: events.Key) -> None:
+#         """Write Key events to log."""
+#         self.terminal.write('\n'+ str(event))
+
+class TerminalButtons(Static):
+    def compose(self)->ComposeResult:
+        with Horizontal():
+            yield Button('Save Log', variant= 'primary', id='save_log')
+            yield Button('Close', variant ='error', id='close') 
+
+class TerminalForm(Static):
+    def compose(self)->ComposeResult:
+        yield Log()
+        yield TerminalButtons()
     @property
-    def terminal(self)->RichLog:
-        return self.query_one(RichLog)
+    def terminal(self)->Log:
+        return self.query_one(Log)
 
 
+
+class TerminalScreen(Screen):
+    def compose(self) -> ComposeResult:
+        yield TerminalForm()
+    @property
+    def terminal(self)->Log:
+        return self.query_one(TerminalForm).terminal
+    
 class RichLogApp(App):
     CSS_PATH = ['terminal.tcss']
     def compose(self) -> ComposeResult:
-        yield Terminal()
-    def on_ready(self) -> None:
+        yield Header()
+        yield Placeholder('giel debiel')
+        yield Footer()
+
+    async def on_mount(self):
+        self.install_screen(TerminalScreen(), name='terminal')
+        await self.app.push_screen('terminal')
         """Called  when the DOM is ready."""
-        terminal = self.query_one(Terminal).terminal
+        terminal = self.query_one(TerminalScreen).terminal
         terminal.write('HALLO WERELD')
 
+    def on_key(self, event: events.Key) -> None:
+        """Write Key events to log."""
+        terminal = self.query_one(TerminalScreen).terminal
+        terminal.write('\n'+ str(event))
+        # terminal.refresh()
         # rows = iter(csv.reader(io.StringIO(CSV)))
         # table = Table(*next(rows))
         # for row in rows:
